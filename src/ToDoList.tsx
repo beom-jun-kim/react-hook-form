a16d136499ca8ab77412962c1baaaf3413a7723d
kimbeomjun <gimb52043@gmail.com>
2023-04-20 15:22:03 +0900

    react-hook-form

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -6,6 +6,7 @@ interface IFormData {
   password: string;
   password2: string;
   address: string;
+  extraError?: string;
 }
 
 function ToDoList() {
@@ -14,19 +15,28 @@ function ToDoList() {
     // watch,
     handleSubmit,
     formState: { errors },
+    setError,
   } = useForm<IFormData>({
     defaultValues: {
       email: "@naver.com",
-    }
+    },
   });
-  const onValid = (data: any) => {
-    console.log("data", data);
+  const onValid = (data: IFormData) => {
+    if (data.password !== data.password2) {
+      setError(
+        "password2",
+        { message: "패스워드가 일치하지 않습니다" },
+
+        // 오류시 focus
+        { shouldFocus: true }
+      );
+    }
+    setError("extraError", { message: "서버가 열리지 않습니다" });
   };
   console.log("errors", errors);
 
   return (
     <div>
-
       {/* email */}
       <form onSubmit={handleSubmit(onValid)}>
         <input
@@ -37,9 +47,9 @@ function ToDoList() {
               message: "네이버 이메일 주소만 가능합니다",
             },
           })}
+          placeholder="이메일"
         />
         <span>{errors?.email?.message}</span>
-
 
         {/* 이름 */}
         <input
@@ -49,11 +59,42 @@ function ToDoList() {
               value: 6,
               message: "6자 이내로 해주세요",
             },
+            validate: {
+              noUsername: (value) => value.includes("김범준") ? "김범준이라는 이름은 가입불가" : true,
+            }
           })}
+          placeholder="이름"
         />
         <span>{errors?.username?.message}</span>
 
+        {/* 비밀번호 */}
+        <input
+          {...register("password", {
+            required: "비밀번호가 입력되지 않았어요",
+            maxLength: {
+              value: 6,
+              message: "6자 이내로 해주세요",
+            },
+          })}
+          placeholder="비밀번호"
+        />
+        <span>{errors?.password?.message}</span>
+
+        {/* 비밀번호 확인 */}
+        <input
+          {...register("password2", {
+            required: "비밀번호 확인이 입력되지 않았어요",
+            maxLength: {
+              value: 6,
+              message: "6자 이내로 해주세요",
+            },
+          })}
+          placeholder="비밀번호 확인"
+        />
+        <span>{errors?.password2?.message}</span>
+
         <button>add</button>
+        <span>{errors?.extraError?.message}</span>
       </form>
     </div>
   );
